1b2a3b1142c54b66d4ca625ce70ebe57498773a1
jeroen-engineer <jeroentheunissen@protonmail.com>
2022-04-28 14:58:14 +0200

    created seperate component for Note, added computed letter count, created delete funcionality

---
+++ src/components/Notes/Note.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="card mb-4">
+    <div class="card-content">
+      <div class="content">
+        {{ note.content }}
+      </div>
+      <div class="has-text-right has-text-grey-light mt-2">
+        <small>{{ characterLength }} </small>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a @click.prevent="handleDeleteClicked" href="#" class="card-footer-item"
+        >Delete</a
+      >
+    </footer>
+  </div>
+</template>
+
+<script setup>
+//IMPORTS
+import { computed } from "vue";
+//PROPS
+const props = defineProps({
+  note: {
+    type: Object,
+    required: true,
+  },
+});
+
+//EMITS
+const emit = defineEmits(["deleteClicked"]);
+
+//CHARACTER LENGTH
+const characterLength = computed(() => {
+  let length = props.note.content.length;
+  let description = length > 1 ? "characters" : "character";
+  return `${length} ${description}`;
+});
+
+//HANDLEDELETECLICKED
+const handleDeleteClicked = () => {
+  emit("deleteClicked", props.note.id);
+};
+</script>
--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -3,30 +3,78 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <Textarea class="textarea" placeholder="Add a Vue note" />
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a Vue note"
+            ref="newNoteRef"
+          />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
+          >
             Add Vue Note
           </button>
         </div>
       </div>
     </div>
-
-    <div v-for="i in 3" class="card mb-4">
-      <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
-          iaculis mauris.
-        </div>
-      </div>
-      <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    <Note
+      v-for="note in notes"
+      :key="note.id"
+      :note="note"
+      @deleteClicked="deleteNote"
+    />
   </div>
 </template>
+
+<script setup>
+// IMPORTS
+import { ref } from "vue";
+import Note from "@/components/Notes/Note.vue";
+
+//NOTES
+const newNote = ref("");
+const newNoteRef = ref(null);
+const notes = ref([
+  {
+    id: "id1",
+    content:
+      "lorem ipsum dolor sit, lorem, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ips, lorem ipsum dolor sit, lorem ips, lorem",
+  },
+  {
+    id: "id2",
+    content:
+      "Short note 2, lorem ipsum dolor sit, lorem, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ips, lorem ipsum dolor sit, lorem ips, lorem",
+  },
+  {
+    id: "id3",
+    content:
+      "Note number 3 lorem ipsum dolor sit, lorem, lorem ipsum dolor sit, lorem ipsum dolor sit, lorem ips, lorem ipsum dolor sit, lorem ips, lorem",
+  },
+]);
+
+const addNote = () => {
+  const currentDate = new Date().getTime();
+  const id = currentDate.toString();
+  const note = {
+    id,
+    content: newNote.value,
+  };
+  notes.value.unshift(note);
+  newNote.value = "";
+  newNoteRef.value.focus();
+};
+
+//DELETE NOTE
+const deleteNote = (idToDelete) => {
+  notes.value = notes.value.filter((note) => {
+    return note.id != idToDelete;
+  });
+};
+</script>
